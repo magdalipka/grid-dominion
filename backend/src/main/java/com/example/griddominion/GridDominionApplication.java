c3d334e72eeee8f460a568be9c4efec6996bc4ee
Magda Lipka <dev@magdalipka.com>
2024-05-07 23:23:38 +0200

    init

---
+++ backend/src/main/java/com/example/griddominion/GridDominionApplication.java
@@ -0,0 +1,13 @@
+package com.example.griddominion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GridDominionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GridDominionApplication.class, args);
+	}
+
+}

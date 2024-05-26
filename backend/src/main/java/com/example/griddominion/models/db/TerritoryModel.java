11f8d4546e3ac9f93fc3611595e1bcbe1da3d976
Magda Lipka <dev@magdalipka.com>
2024-05-26 12:29:49 +0200

    fix territory id conflict error

--- backend/src/main/java/com/example/griddominion/models/db/TerritoryModel.java
+++ backend/src/main/java/com/example/griddominion/models/db/TerritoryModel.java
@@ -4,6 +4,8 @@ package com.example.griddominion.models.db;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -13,6 +15,7 @@ import jakarta.persistence.Table;
 @Table(name = "territories")
 public class TerritoryModel {
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private int id;
 
     @ManyToOne(fetch = FetchType.LAZY)

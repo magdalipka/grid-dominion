57b1f23b812dd9184c82fcd415c5df6abb08be97
Magda Lipka <dev@magdalipka.com>
2024-06-02 00:22:26 +0200

    send current location

--- frontend/components/TerritoryDetails.tsx
+++ frontend/components/TerritoryDetails.tsx
@@ -3,6 +3,7 @@ import { Image, StyleSheet, View, Text, Button } from "react-native";
 import GoldIcon from "@/assets/icons/gold.svg";
 import WoodIcon from "@/assets/icons/wood.svg";
 import FoodIcon from "@/assets/icons/food.svg";
+import MeepleIcon from "@/assets/icons/meeple.svg";
 
 const ICON_SIZE = 24;
 
@@ -12,18 +13,27 @@ export const TerritoryDetails = ({ territory }: { territory?: Territory }) => {
       {Boolean(territory?.ownerNick) && (
         <Text style={styles.header}>Owned by {territory?.ownerNick}</Text>
       )}
-      <View style={styles.resourcesContainer}>
-        <View style={styles.resource}>
-          <GoldIcon width={ICON_SIZE} height={ICON_SIZE} />
-          <Text style={styles.resourceText}>{territory?.gold}</Text>
+      <Text>{JSON.stringify(territory)}</Text>
+      <View style={styles.detailsContainer}>
+        <View style={styles.resourcesContainer}>
+          <View style={styles.resource}>
+            <GoldIcon width={ICON_SIZE} height={ICON_SIZE} />
+            <Text style={styles.resourceText}>{territory?.gold || 0}</Text>
+          </View>
+          <View style={styles.resource}>
+            <WoodIcon width={ICON_SIZE} height={ICON_SIZE} />
+            <Text style={styles.resourceText}>{territory?.wood || 0}</Text>
+          </View>
+          <View style={styles.resource}>
+            <FoodIcon width={ICON_SIZE} height={ICON_SIZE} />
+            <Text style={styles.resourceText}>{territory?.food || 0}</Text>
+          </View>
         </View>
-        <View style={styles.resource}>
-          <WoodIcon width={ICON_SIZE} height={ICON_SIZE} />
-          <Text style={styles.resourceText}>{territory?.wood}</Text>
-        </View>
-        <View style={styles.resource}>
-          <FoodIcon width={ICON_SIZE} height={ICON_SIZE} />
-          <Text style={styles.resourceText}>{territory?.food}</Text>
+        <View style={styles.resourcesContainer}>
+          <View style={styles.army}>
+            <Text style={styles.resourceText}>{territory?.minions || 0}</Text>
+            <MeepleIcon width={ICON_SIZE} height={ICON_SIZE} />
+          </View>
         </View>
       </View>
     </View>
@@ -40,22 +50,27 @@ const styles = StyleSheet.create({
     alignContent: "center",
     fontSize: 18,
     margin: 16,
-    borderWidth: 1,
-    borderStyle: "dashed",
-    borderColor: "green",
+  },
+  detailsContainer: {
+    flex: 1,
+    flexDirection: "row",
+    padding: 16,
   },
   resourcesContainer: {
     flex: 1,
     width: "100%",
     display: "flex",
     gap: 8,
-    borderWidth: 1,
-    borderStyle: "solid",
-    borderColor: "red",
   },
   resource: {
     display: "flex",
     flexDirection: "row",
+    gap: 8,
+  },
+  army: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "flex-end",
     gap: 8,
   },
   resourceText: {

d21967e982d8268420e7735889c54375e10bf44e
Magda Lipka <dev@magdalipka.com>
2024-06-02 12:38:16 +0200

    fix clan admin

--- backend/src/main/java/com/example/griddominion/models/api/output/UserOutput.java
+++ backend/src/main/java/com/example/griddominion/models/api/output/UserOutput.java
@@ -1,19 +1,23 @@
 package com.example.griddominion.models.api.output;
 
+import com.example.griddominion.models.db.InventoryModel;
 import com.example.griddominion.models.db.UserModel;
 
 public class UserOutput {
   public String id;
   public String nick;
+  public InventoryModel inventory;
 
   public UserOutput(UserModel userModel) {
     this.id = userModel.getId();
     this.nick = userModel.getNick();
+    this.inventory = userModel.getInventory();
   }
 
-  public UserOutput(String id, String nick) {
+  public UserOutput(String id, String nick, InventoryModel inventory) {
     this.id = id;
     this.nick = nick;
+    this.inventory = inventory;
   }
 
 }

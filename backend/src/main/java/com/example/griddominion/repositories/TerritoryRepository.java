b045eae3e357b32b8539e9ae38b42a53a71bf48d
Szymon Lutowicz <szy251@wp.pl>
2024-06-10 21:48:54 +0200

    fight updated

--- backend/src/main/java/com/example/griddominion/repositories/TerritoryRepository.java
+++ backend/src/main/java/com/example/griddominion/repositories/TerritoryRepository.java
@@ -3,5 +3,5 @@ package com.example.griddominion.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.griddominion.models.db.TerritoryModel;
 
-public interface TerritoryRepository extends JpaRepository<TerritoryModel, String> {
+public interface TerritoryRepository extends JpaRepository<TerritoryModel, Integer> {
 }
